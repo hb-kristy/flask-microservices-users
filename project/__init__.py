ba2e08b1ea6038ab21ef0a7982377b115d7aa9b6
Kristy Morales <kristy@thehelperbees.com>
2017-12-04 13:17:50 -0600

    part 2 commit

--- project/__init__.py
+++ project/__init__.py
@@ -1,19 +1,27 @@
 # project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# instantiate the db
+db = SQLAlchemy()
 
 
-# instantiate the app
-app = Flask(__name__)
+def create_app():
 
+    # instantiate the app
+    app = Flask(__name__)
+    
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+        
+    # set up extensions
+    db.init_app(app)    
 
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+    # register blueprints
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    return app
